f457d3aa5d6962f492d3951d4f9a15483bba4247
bernabasa <75853223+bernabasa@users.noreply.github.com>
2022-12-08 11:59:05 -0500

    porfolio started

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-    }
-    console.log('==');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-    // is the same as this ....
-    profileDataArr.forEach(profileItem => 
-        console.log(profileItem));
-    
-};
 
-printProfileData(profileDataArgs);
 
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
